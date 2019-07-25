322fc6c6c090eb211c8fffd967c43faf3845a5b2
Jorge Zuluaga <jorge.zuluaga@udea.edu.co>
2019-07-25 15:01:03 -0500

    Commit

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='grt',  
+     name='gravray',  
      version='0.1',
-     scripts=['dokr'] ,
+     scripts=['grt.py'] ,
+     test_suite='nose.collector',
+     tests_require=['nose'],
      author="Jorge I. Zuluaga",
      author_email="jorge.zuluaga@udea.edu.co",
      description="Gravitational Ray Tracing",
@@ -19,4 +21,5 @@ setuptools.setup(
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
+     include_package_data=True,
  )
